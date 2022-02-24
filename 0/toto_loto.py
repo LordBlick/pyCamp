6be06a092fa7d57fa3612388edbc4c74c4314150
LordBlick <LordBlick@gmail.com>
2022-02-24 01:22:02 +0100

    More werbose presentation in task#0

--- 0/toto_loto.py
+++ 0/toto_loto.py
@@ -48,11 +48,32 @@ def symul():
         dict_counts[6] += 1
     return dict_counts
 
+def str_int(_int):
+    """Format big integer"""
+    return f"{_int:,d}".replace(',', ' ')
+
+def weeks_to_years(weeks):
+    """Calculation of Years from Weeks"""
+    days = weeks*7
+    years = days//365
+    str_out = ''
+    if years:
+        str_out += f"{str_int(years)} lat" if years>1 else 'rok'
+    rest_wks = days%365
+    if years and rest_wks:
+        str_out += ' i '
+    if rest_wks:
+        str_out += f"{str_int(rest_wks)} tygodni" if rest_wks>1 else ' tygodnia'
+    return str_out
+
 def main():
     """Main Routine"""
     score = symul()
     print("Wylosowanie dokładnie założonych liczb:"
-        f" {', '.join(str(d) for d in MY_BET)} zajęło {score[6]} prób.")
+        f" {', '.join(str(d) for d in MY_BET)} zajęło {str_int(score[6])} prób.")
+    print("Przy założeniu 3 PLN za zakład, całkowicie wydane pieniądze"
+        f" na zakłady: {str_int(score[6]*3)} PLN")
+    print(f"Przy 1 zakładdzie na tydzień, wygrana zajmie {weeks_to_years(score[6])}")
     for test in range(3, 6):
         print(f"W ich trakcie wypadło {score[test]:d} razy {test:d}")
 
